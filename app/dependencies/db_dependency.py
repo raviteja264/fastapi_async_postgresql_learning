e822b23ccf8f5b665820192e0ee3aa037edee750
Pasupuleti Raviteja <pasupuletiraviteja264@gmail.com>
2026-05-30 20:02:36 +0530

    feat(db_dependency): implement async db_dependency layer

---
+++ app/dependencies/db_dependency.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database import AsyncSessionLocal
+from collections.abc import AsyncGenerator
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    logger.info("Creating new database session")
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            logger.info("Closing database session")
