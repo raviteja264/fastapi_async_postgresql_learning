ea0878e87c6c42b1ca7914a9bb3ef0af4612c221
Pasupuleti Raviteja <pasupuletiraviteja264@gmail.com>
2026-05-31 09:36:37 +0530

    feat(models): Added a user model

---
+++ app/models/user_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
